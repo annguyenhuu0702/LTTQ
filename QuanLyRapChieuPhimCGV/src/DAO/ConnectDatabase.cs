29254c68154e9bd953389dc6675ddf770ba34df4
Nguyễn Hữu An <nguyenhuuan2727@gmail.com>
2022-01-10 19:11:44 +0700

    first commit

---
+++ QuanLyRapChieuPhimCGV/src/DAO/ConnectDatabase.cs
@@ -0,0 +1,9 @@
+﻿using System.Data.SqlClient;
+
+namespace QuanLyRapChieuPhimCGV.src.DAO
+{
+    public class ConnectDatabase
+    {
+        public SqlConnection cnn = new SqlConnection(@"Data Source=TOPXOAN\SQLEXPRESS;Initial Catalog=QLRapChieuPhimCGV;User ID=sa;Password=0384974745");
+    }
+}
